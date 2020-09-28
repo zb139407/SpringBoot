96c06ee5b53e03f757dcc8bf823fd9b19d19367b
zhangbin <1394078687@qq.coom>
2020-09-29 00:06:44 +0800

    添加springboot测试类

---
+++ springboot-first/src/test/java/com/binfen/SpringBootFirstApplicationTests.java
@@ -0,0 +1,17 @@
+package com.binfen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * @author: zhangBin 1394078687@qq.com
+ * @description: springboot测试类
+ * @date: 2020/9/29 0:04
+ */
+@SpringBootTest
+public class SpringBootFirstApplicationTests {
+    @Test
+    void contextLoads() {
+//        System.out.println(1);
+    }
+}
